f34ace3edbe64ac37912564d467e9591f807ed82
Michael Croteau <MichaelCroteau@dhcp-210-201.wireless.bowdoin.edu>
2014-11-09 18:18:11 -0500

    .c and .h files updated with code and general algorithm framework. May not compile yet.

--- simplification.h
+++ simplification.h
@@ -2,14 +2,36 @@
 //Authors: Croteau, Imhoff, Zeller
 //Date: 11/09/14
 
-//define the struct Grid
+
 typedef struct _grid {
-  int rows, cols;   //dimensions of grid
-  int** data;     //2D array of values in grid
+    int rows, cols;     //dimensions of grid
+    int** data;         //2D array of values in grid
 } Grid;
 
+
+typedef struct _color {
+    int r, g, b;
+} Color;
+
+
+typedef struct _vertex {
+    int row, col, value;    ///value will become Color///
+} Vertex;
+
+
+typedef struct _triangle {
+    Vertex * v1, v2, v3;    //vertices of triangle
+    Triangle * t1, t2, t3;  //adjacent triangles
+} Triangle;
+
+
+typedef struct _tin {
+    Triangle * triangle;     //any triangle in the TIN
+} TIN;
+
+
 //This method sets the element at (row, col) in a grid to value.
-void set(Grid *g, int row, int col, int value);
+void set(Grid *g, int row, int col, int value);     ///will set a Color///
 
 //This method gets the value at (row, col) in a grid.
-float get(Grid g, int row, int col);
+int get(Grid g, int row, int col);      ///will get a Color///
