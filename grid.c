2fe9fb045faad785eb791f074573b6e1995a3415
Josh Imhoff <joshimhoff13@gmail.com>
2014-11-09 16:28:58 -0500

    Adds grid structure files

---
+++ grid.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "viewshed.h"
+
+int allocateGrid(Grid* grid, int nrows, int ncols)
+{
+    int i;
+
+    if ((grid->values = malloc(nrows*sizeof(int *))) == NULL)
+        return 0;
+    for (i = 0; i < nrows; i++) {
+        if ((grid->values[i] = malloc(ncols*sizeof(int))) == NULL)
+            return 0;
+    }
+
+    return 1;
+}
+
+void freeGrid(Grid* grid)
+{
+    int i;
+
+    for (i = 0; i < grid->nrows; i++) {
+        free(grid->values[i]);
+    }
+    free(grid->values);
+}
+
+int readFileIntoGrid(Grid* grid, char* filename)
+{
+    int i, j;
+
+    FILE* f;
+    if ((f = fopen(filename, "r")) == NULL)
+        return 0;
+
+    fscanf(f, "ncols %d\n", &grid->ncols);
+    fscanf(f, "nrows %d\n", &grid->nrows);
+
+    if (!allocateGrid(grid, grid->nrows, grid->ncols))
+        return 0;
+
+    fscanf(f, "xllcorner %d\n", &grid->xllcorner);
+    fscanf(f, "yllcorner %d\n", &grid->yllcorner);
+    fscanf(f, "cellsize %d\n", &grid->cellsize);
+    fscanf(f, "NODATA_value %d\n", &grid->NODATA_value);
+
+    for (i = 0; i < grid->nrows; i++) {
+        for (j = 0; j < grid->ncols; j++) {
+            fscanf(f, "%d", &(grid->values[i][j]));
+        }
+    }
+
+    return 1;
+}
+
+int saveGridToFile(Grid* grid, char* filename)
+{
+    int i, j;
+
+    FILE* f;
+    if ((f = fopen(filename, "w")) == NULL)
+        return 0;
+
+    fprintf(f, "ncols %d\n", grid->ncols);
+    fprintf(f, "nrows %d\n", grid->nrows);
+    fprintf(f, "xllcorner %d\n", grid->xllcorner);
+    fprintf(f, "yllcorner %d\n", grid->yllcorner);
+    fprintf(f, "cellsize %d\n", grid->cellsize);
+    fprintf(f, "NODATA_value %d\n", grid->NODATA_value);
+
+    for (i = 0; i < grid->nrows; i++) {
+        for (j = 0; j < grid->ncols; j++) {
+            fprintf(f, "%d ", grid->values[i][j]);
+        }
+        fprintf(f, "\n");
+    }
+
+    freeGrid(grid);
+    return 1;
+}
+
+void copyGridHeader(Grid* to, Grid* from)
+{
+    to->ncols = from->ncols;
+    to->nrows = from->nrows;
+    to->xllcorner = from->xllcorner;
+    to->yllcorner = from->yllcorner;
+    to->cellsize = from->cellsize;
+    to->NODATA_value = from->NODATA_value;
+}
+
+void printGrid(Grid* grid)
+{
+    int i, j;
+
+    printf("Printing grid with %d cols and %d rows.\n", grid->ncols, grid->nrows);
+    for (i = 0; i < grid->nrows; i++) {
+        for (j = 0; j < grid->ncols; j++) {
+            printf("%d ", grid->values[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int inBoundsOnGrid(Grid* grid, int i, int j)
+{
+    return (i >= 0 && j >= 0 && i < grid->nrows && j < grid->ncols);
+}
+
+int noDataAtPoint(Grid* grid, int i, int j)
+{
+    return (grid->NODATA_value == grid->values[i][j]);
+}
---
+++ grid.h
@@ -0,0 +1,51 @@
+// Grid structure designed to store in memory what the fields in a .asc file
+typedef struct _grid {
+    int ncols, nrows, xllcorner, yllcorner, cellsize, NODATA_value;
+    int** values;
+} Grid;
+
+// Allocates a two dimensional array of ints to be used by the Grid struct
+// @param grid, a pointer to a grid structure with the two dimensional array unallocated
+// @paran nrows, the size of the array of int *
+// @param ncols, the size of the arrays of ints
+// @returns 0 if failure, 1 if successful
+int allocateGrid(Grid* grid, int nrows, int ncols);
+
+// Frees the two dimensional array used by the Grid struct
+// @param grid, a pointer to a grid structure with the two dimensional array allocated
+void freeGrid(Grid* grid);
+
+// Read .asc file into a Grid structure
+// @param grid, a pointer to the grid structure to fill with data
+// @param filename, the name of the file on the filesystem
+// @returns 0 if failure, 1 if successful
+int readFileIntoGrid(Grid* grid, char* filename);
+
+// Save Grid structure as .asc file
+// @param grid, a pointer to the grid structure to be saved to the filesystem
+// @param filename, the name of the file on the filesystem
+// @returns 0 if failure, 1 if successful
+int saveGridToFile(Grid* grid, char* filename);
+
+// Copies all fields except values of the from grid to the to grid
+// @param to, a pointer to the grid that we are copying the header to
+// @param from, a pointer to the grid that we are copying the header from
+void copyGridHeader(Grid* to, Grid* from);
+
+// Print the grid
+// @param grid, a pointer to the grid structure to be printed
+void printGrid(Grid* grid);
+
+// Check whether (i,j) is a valid index into the grid
+// @param grid, a pointer to the grid to check bounds on
+// @param i, is grid->values[i][j] within the bounds of the two dim array?
+// @param j, is grid->values[i][j] within the bounds of the two dim array?
+// @returns a boolean indicating whether (i,j) is a valid index
+int inBoundsOnGrid(Grid* grid, int i, int j);
+
+// Check whether there is actual data at point (i,j) on grid
+// @param grid, a pointer to the grid to check
+// @param i, is grid->values[i][j] equal to grid->NODATA_value
+// @param j, is grid->values[i][j] equal to grid->NODATA_value
+// @returns a boolean indicating whether there is actual data at point (i,j)
+int noDataAtPoint(Grid* grid, int i, int j);
